514419a679606ee3f8db7393d1e2253fb1b5a4f9
Kyle Printz <kyleprintz@Kyles-MacBook-Pro-2.local>
2016-06-03 12:47:49 -0700

    initial code dump

---
+++ app.js
@@ -0,0 +1,127 @@
+'use strict';
+
+const fs = require('fs');
+const Absurd = require('absurd');
+const JefNode = require('json-easy-filter').JefNode;
+const absurd = Absurd();
+const _ = require('lodash');
+
+
+fs.readFile('style.json', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    const jsonTree = JSON.parse(data);
+    const hierarchyTree = jsonTree.hierarchy;
+    let depth = 0;
+    let h_depth = 0;
+
+    // transform html hierarchy here.
+    let div_array = [];
+
+    let open_depth = -1;
+    let variable_array = [];
+
+    const res = new JefNode(hierarchyTree).filter((node) => {
+
+        if (node.has('type')) {
+            const t = node.value;
+            const n = node.key;
+            let base_class = n.replace("."," ");
+            base_class = base_class.replace("."," ");
+            console.log("node name = "+base_class);
+            if ((t.type == 'div') || (t.type == 'text')) {              // or we can test for 'icon' etc.
+                console.log(`div id - ${t.id}`);
+                console.log(`div style ${t.style_class}`);
+                console.log(`hierarchy depth ${depth}`);
+                if(open_depth != depth) {
+                    div_array.push(`<div id="${t.id}" class="${t.style_class}${base_class}">`);
+                    open_depth = depth;
+                }
+                // we know its a div here, and we know the hierarchy depth (depth) so we can do div within div
+
+                if (node.level > h_depth) {
+                    h_depth = node.level;
+                    depth++;
+                }
+                if (node.level < h_depth) {
+                    h_depth = node.level;
+                    depth--;
+                }
+            }
+        }
+    });
+    let iter = 0;
+    div_array.forEach(function (d) {
+        let output_string = " ".repeat(iter * 4) + d;
+        iter++;
+        console.log(output_string);
+    });
+    iter--;
+    for (let i = 0; i < div_array.length; i++) {
+        let output_string = " ".repeat(iter * 4) + '</div>'
+        iter--;
+        console.log(output_string);
+    }
+
+    const styleTree = jsonTree.style;
+
+    // transformation style  here.
+    absurd.hook("add", function (rules) {
+        const res = new JefNode(rules).filter((node) => {
+            // store variables..
+            if (node.isRoot) {
+                let val = node.value;
+                if (val) {
+                    for (let i in val) {
+                        let p = i;
+                        if (p.indexOf('$') > -1) {
+                            let var_val = val[i];
+                            variable_array.push({ var: p, value: var_val });
+                            delete rules[p];
+                        }
+                    }
+                }
+            }
+            if (!node.isRoot && !node.isLeaf) {
+                let val = node.value;
+                if (val) {
+                    for (let i in val) {
+                        const variable_reference = val[i];
+                        if (variable_reference.indexOf('$') > -1) {
+                            const p = _.find(variable_array, function (o) { return o.var == variable_reference; });
+                            // look up the variable.
+                            val[i] = p.value;
+                        }
+                    }
+                }
+            }
+
+
+            if (node.has('font-size')) {
+                const t = node.value;
+                let v = t['font-size'];
+                v = v.replace('px', '');
+                t['font-size'] = v;
+            }
+            // above will remove px from all font-sizes;
+            // you can of course do it gloally. uncomment
+            // if (!node.isRoot && !node.isLeaf) {
+            //     let t2 = node.value;
+            //     if (t2) {
+            //         for (let i in t2) {
+            //             if (t2[i].indexOf('px' > -1)) {
+            //                 t2[i] = t2[i].replace('px', '')
+            //             }
+            //         }
+            //     }
+            // }
+        })
+    });
+
+    absurd.add(styleTree).compile((err, css) => {
+        // output the css to disk for web here
+        console.log(css);
+    });
+
+});
